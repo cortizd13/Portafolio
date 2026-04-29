e49beb1fc4d343fa46bf7e05be5a2b74e0c9eb58
Camilo <ortizdiazcamiloandres@gmail.com>
2026-04-29 10:05:12 -0500

    Agregando menú hamburguesa en responsive

---
+++ script.js
@@ -0,0 +1,13 @@
+const menuBtn = document.querySelector(".menu");
+const linksContainer = document.querySelector(".link-container");
+const links = document.querySelectorAll(".link")
+
+menuBtn.addEventListener("click",()=>{
+    linksContainer.classList.toggle("open");
+})
+
+links.forEach(link =>{
+    link.addEventListener("click",()=>{
+        linksContainer.classList.remove("open");
+    })
+})
